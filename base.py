f5959f85e65c92fd5a637450c61e943cef254e89
Tysonzero <danielspencersmith@gmail.com>
2014-01-29 20:52:21 -0700

    Validate input and add setDigits function

--- base.py
+++ base.py
@@ -1,23 +1,31 @@
 import math
 
-DIGITS = {
+digits = {
 	0:'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 	2:'01',
 	10:'0123456789',
 	16:'0123456789ABCDEF',
 }
 
+def setDigits(value, base=0):
+	if base < 0 or base == 1:
+		raise ValueError, 'Base must be an integer greater than or equal to 2 or 0 to set default.'
+	digits[base] = value
+
 def convert(value, initial, terminal):
 	return fromNumber(toNumber(value, initial), terminal)
 
 def toNumber(value, base):
+	if int(base) < 2 or int(base) != base:
+		raise ValueError, 'Base must be an integer greater than or equal to 2.'
+
 	value = str(value)
 	number = 0
 	i = 0
 
 	for char in reversed(value):
-		if DIGITS.find(char) != -1 and DIGITS.find(char) < base:
-			number += DIGITS.find(char) * base**i
+		if digits.find(char) != -1 and digits.find(char) < base:
+			number += digits.find(char) * base**i
 
 		i += 1
 
@@ -25,6 +33,9 @@ def toNumber(value, base):
 
 
 def fromNumber(number, base):
+	if int(base) < 2 or int(base) != base:
+		raise ValueError, 'Base must be an integer greater than or equal to 2.'
+
 	value = ''
 	i = 0
 
@@ -32,7 +43,7 @@ def fromNumber(number, base):
 		for i in reversed(xrange(1 + int(round(math.log(number, base), 10)))):
 			x = int(number / base**i)
 			number -= x * base**i
-			value += DIGITS[x]
+			value += digits[x]
 	except ValueError:
 		value = '0'
 
