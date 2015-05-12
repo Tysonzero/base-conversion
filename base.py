cb2fae6d85d5400cd8e105fee493eedd00e43ac0
Tysonzero <danielspencersmith@gmail.com>
2015-05-11 23:51:37 -0600

    Rename digits to digit_strings

--- base.py
+++ base.py
@@ -1,4 +1,4 @@
-digits = {
+digit_strings = {
     None: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 }
 
@@ -16,7 +16,7 @@ def int_log(number, base):
 
 
 def set_digits(value, base=None):
-    digits[int(base)] = str(value)
+    digit_strings[int(base)] = str(value)
 
 
 def set_separators(value, base=None):
@@ -26,31 +26,31 @@ def set_separators(value, base=None):
 def to_number(value, base):
     value = str(value)
     base = int(base)
-    d = base if digits.get(base) else None
+    d = base if digit_strings.get(base) else None
     s = base if separators.get(base) else None
     decimal = value.find(separators[s])
     decimal = len(value) - 1 if decimal == -1 else decimal - 1
     value = value.replace(separators[s], '')
     number = 0
     for i, char in enumerate(value):
-        if digits[d].find(char) != -1 and digits[d].find(char) < base:
-            number += digits[d].find(char) * base**(decimal - i)
+        if digit_strings[d].find(char) != -1 and digit_strings[d].find(char) < base:
+            number += digit_strings[d].find(char) * base**(decimal - i)
     return number
 
 
 def to_base(number, base, precision=0):
     number = float(number) if precision else int(number)
     base = int(base)
-    d = base if digits.get(base) else None
+    d = base if digit_strings.get(base) else None
     s = base if separators.get(base) else None
     value = ''
     try:
         for i in reversed(xrange(precision + 1 + int_log(number, base))):
             x = int(number / base**(i - precision))
             number -= x * base**(i - precision)
-            value += digits[d][x]
+            value += digit_strings[d][x]
     except ValueError:
-        value = digits[d][0]
+        value = digit_strings[d][0]
     if precision:
         value = value[:len(value) - precision] + separators[s] + value[len(value) - precision:]
     return value
