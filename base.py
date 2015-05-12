94dd6b8171e48f23375ed2c9ecaa0013aa5459cc
Tysonzero <danielspencersmith@gmail.com>
2015-05-11 23:56:30 -0600

    Replace items[i] with item for separators / digits

--- base.py
+++ base.py
@@ -26,33 +26,33 @@ def set_separators(value, base=None):
 def to_number(value, base):
     value = str(value)
     base = int(base)
-    d = base if digit_strings.get(base) else None
-    s = base if separators.get(base) else None
-    decimal = value.find(separators[s])
+    digit_string = digit_strings.get(base) or digit_strings[None]
+    separator = separators.get(base) or separators[None]
+    decimal = value.find(separator)
     decimal = len(value) - 1 if decimal == -1 else decimal - 1
-    value = value.replace(separators[s], '')
+    value = value.replace(separator, '')
     number = 0
     for i, char in enumerate(value):
-        if digit_strings[d].find(char) != -1 and digit_strings[d].find(char) < base:
-            number += digit_strings[d].find(char) * base**(decimal - i)
+        if digit_string.find(char) != -1 and digit_string.find(char) < base:
+            number += digit_string.find(char) * base**(decimal - i)
     return number
 
 
 def to_base(number, base, precision=0):
     number = float(number) if precision else int(number)
     base = int(base)
-    d = base if digit_strings.get(base) else None
-    s = base if separators.get(base) else None
+    digit_string = digit_strings.get(base) or digit_strings[None]
+    separator = separators.get(base) or separators[None]
     value = ''
     try:
         for i in reversed(xrange(precision + 1 + int_log(number, base))):
             x = int(number / base**(i - precision))
             number -= x * base**(i - precision)
-            value += digit_strings[d][x]
+            value += digit_string[x]
     except ValueError:
-        value = digit_strings[d][0]
+        value = digit_string[0]
     if precision:
-        value = value[:len(value) - precision] + separators[s] + value[len(value) - precision:]
+        value = value[:len(value) - precision] + separator + value[len(value) - precision:]
     return value
 
 
