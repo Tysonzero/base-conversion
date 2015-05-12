712588d10f86230488c1912b19deb9e20b199adc
Tysonzero <danielspencersmith@gmail.com>
2015-05-11 23:59:10 -0600

    Fix issue where you couldn't set defaults

--- base.py
+++ base.py
@@ -1,10 +1,10 @@
 digit_strings = {
-    None: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    0: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 }
 
 
 separators = {
-    None: '.',
+    0: '.',
 }
 
 
@@ -15,19 +15,19 @@ def int_log(number, base):
     return i - 1
 
 
-def set_digits(value, base=None):
+def set_digits(value, base=0):
     digit_strings[int(base)] = str(value)
 
 
-def set_separators(value, base=None):
+def set_separators(value, base=0):
     separators[int(base)] = str(value)
 
 
 def to_number(value, base):
     value = str(value)
     base = int(base)
-    digit_string = digit_strings.get(base) or digit_strings[None]
-    separator = separators.get(base) or separators[None]
+    digit_string = digit_strings.get(base) or digit_strings[0]
+    separator = separators.get(base) or separators[0]
     decimal = value.find(separator)
     decimal = len(value) - 1 if decimal == -1 else decimal - 1
     value = value.replace(separator, '')
@@ -41,8 +41,8 @@ def to_number(value, base):
 def to_base(number, base, precision=0):
     number = float(number) if precision else int(number)
     base = int(base)
-    digit_string = digit_strings.get(base) or digit_strings[None]
-    separator = separators.get(base) or separators[None]
+    digit_string = digit_strings.get(base) or digit_strings[0]
+    separator = separators.get(base) or separators[0]
     value = ''
     try:
         for i in reversed(xrange(precision + 1 + int_log(number, base))):
