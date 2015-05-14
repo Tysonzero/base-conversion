58ddca47521e33b94478f7aecb21c8f3d9185eb7
Tysonzero <danielspencersmith@gmail.com>
2015-05-13 23:44:53 -0700

    Fix PEP8 violations

--- base1.py
+++ base1.py
@@ -1,20 +1,24 @@
 import math
 
+
 digits = {
-    0:'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    0: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 }
+
 
 def setDigits(value, base=0):
     if base < 0 or base == 1:
-        raise ValueError, 'Base must be an integer greater than or equal to 2 or 0 to set default.'
+        raise ValueError('Base must be an integer greater than or equal to 2 or 0 to set default.')
     digits[base] = value
+
 
 def convert(value, initial, terminal):
     return fromNumber(toNumber(value, initial), terminal)
 
+
 def toNumber(value, base):
     if int(base) < 2 or int(base) != base:
-        raise ValueError, 'Base must be an integer greater than or equal to 2.'
+        raise ValueError('Base must be an integer greater than or equal to 2.')
 
     value = str(value)
     number = 0
@@ -22,7 +26,7 @@ def toNumber(value, base):
     b = 0
 
     if digits.get(base, ''):
-        b = base 
+        b = base
 
     for char in reversed(value):
         if digits[b].find(char) != -1 and digits[b].find(char) < base:
@@ -35,14 +39,14 @@ def toNumber(value, base):
 
 def fromNumber(number, base):
     if int(base) < 2 or int(base) != base:
-        raise ValueError, 'Base must be an integer greater than or equal to 2.'
+        raise ValueError('Base must be an integer greater than or equal to 2.')
 
     value = ''
     i = 0
     b = 0
 
     if digits.get(base, ''):
-        b = base 
+        b = base
 
     try:
         for i in reversed(xrange(1 + int(round(math.log(number, base), 10)))):
